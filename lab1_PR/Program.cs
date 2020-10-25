e6449caff392bcb97fc0553018feb7a0cebc64dd
Dina Ciorba <ciorbadina9@gmail.com>
2020-10-25 21:15:25 +0200

    add lab 1

---
+++ lab1_PR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_PR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
