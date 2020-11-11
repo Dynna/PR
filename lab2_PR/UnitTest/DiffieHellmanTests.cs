6f5cc4d1b36e4a2aafc38fcade2b9aacc1b01ff3
Dina Ciorba <ciorbadina9@gmail.com>
2020-11-11 03:40:49 +0200

    add test for Diffie Hellman encrypt and decrypt methods

---
+++ lab2_PR/UnitTest/DiffieHellmanTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+
+namespace SecureKeyExchange.Tests
+{
+    [TestClass()]
+    class DiffieHellmanTests
+    {
+        internal static string msg;
+
+        [TestMethod()]
+        public void Encrypt_Decrypt()
+        {
+            string text = "Test message!";
+
+            using (var client = new DiffieHellman())
+            {
+                using (var server = new DiffieHellman())
+                {
+                    // client uses server's public key to encrypt his message
+                    byte[] secretMessage = client.Encrypt(server.PublicKey, text);
+
+                    // server uses client's public key and IV to decrypt the secret message
+                    string decryptedMessage = server.Decrypt(client.PublicKey, secretMessage, client.IV);
+                }
+            }
+        }
+    }
+}
---
+++ lab2_PR/UnitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using SecureKeyExchange.Tests;
+using System;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string e = DiffieHellmanTests.msg;
+
+            Console.WriteLine(e);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
